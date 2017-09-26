60f748c157cc61a8625cd719ca7f9bd089b4504e
Ania Brajtkopf <aniabrajtkopf@gmail.com>
2017-09-26 12:39:56 +0200

    add mongoose, user schema and passport strategy

---
+++ passport.js
@@ -0,0 +1,37 @@
+const passport = require('passport');
+const SpotifyStrategy = require('passport-spotify').Strategy;
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    });
+});
+
+passport.use(
+  new SpotifyStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: '/auth/spotify/callback'
+  }, 
+  (accessToken, refreshToken, profile, done) => {
+    User.findOne({ spotifyId: profile.id })
+      .then(existingUser => {
+        if (existingUser) {
+          // user is already in database
+          done(null, existingUser, { refreshToken, accessToken });
+        } else {
+          // save new user
+          new User({ spotifyId: profile.id, username: profile.username })
+            .save()
+            .then(user => done(null, user, { refreshToken, accessToken }));
+        }
+      });
+  })
+);
--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 
-require('dotenv').config({ path: 'variables.env' });
-
-
 const app = express();
 
+mongoose.connect(process.env.DATABASE, {
+  useMongoClient: true
+});
+mongoose.Promise = global.Promise;
+
+require('dotenv').config({ path: 'variables.env' });
+require('./models/User');
+require('./passport');
+
+app.use(session({
+  secret: process.env.SECRET,
+  key: process.env.KEY,
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({ 
+    mongooseConnection: mongoose.connection 
+  })
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 const PORT = process.env.PORT || 8080;
