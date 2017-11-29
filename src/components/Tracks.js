87d139d9e36c0357e83be74d19c1a13c3e269ef4
Ania Brajtkopf <aniabrajtkopf@gmail.com>
2017-11-29 19:52:16 +0100

    added css grid

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -4,59 +4,59 @@ import { connect } from 'react-redux';
 import { fetchTracks } from '../actions';
 
 class Tracks extends Component {
-	fetchTracks() {
-		if (this.props.tracks.length >= 50) {
-			return;
-		}
-		this.props.fetchTracks(this.props.tracks.length);
-	}
+  fetchTracks() {
+    if (this.props.tracks.length >= 50) {
+      return;
+    }
+    this.props.fetchTracks(this.props.tracks.length);
+  }
 
-	render() {
-		let tracks = [];
-		this.props.tracks.map((track, i)=> {
-			tracks.push(
-				<div className="card" key={track.id}>
-					<div className="card__image">
-						<div className="card__image__link">
-							<a href={`https://open.spotify.com/track/${track.id}`} target="_blank">PLAY</a>
-						</div>
-						<img src={track.image} alt={track.title} />
-					</div>
-					<div className="card__block">
-						<div className="card__block__title">
-							{i + 1}. {track.title}
-						</div>
-						<div className="card__block__text">
-							<div className="card__block__text__artist">{track.artist}</div>
-							<div className="card__block__text__album">{track.album}</div>
-						</div>
-					</div>
-				</div>
-			);
-		});
-		return (
-			<div className="container">
-				<div className="site-title">Your Top Tracks</div>
-					<InfiniteScroll
-						className="cards"
-						pageStart={0}
-						loadMore={this.fetchTracks.bind(this)}
-						hasMore={this.props.tracks.length <= 40 && this.props.hasMore}
-						loader={<div className="loader">Loading...</div>}
-						threshold={500}
-					>
-						{tracks}
-					</InfiniteScroll>
-			</div>
-		);
-	}
+  render() {
+    let tracks = [];
+    this.props.tracks.map((track, i)=> {
+      tracks.push(
+        <div className="card" key={track.id}>
+          <div className="card__image">
+            <div className="card__image__link">
+              <a href={`https://open.spotify.com/track/${track.id}`} target="_blank">PLAY</a>
+            </div>
+            <img src={track.image} alt={track.title} />
+          </div>
+          <div className="card__block">
+            <div className="card__block__title">
+              {i + 1}. {track.title}
+            </div>
+            <div className="card__block__text">
+              <div className="card__block__text__artist">{track.artist}</div>
+              <div className="card__block__text__album">{track.album}</div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+    return (
+      <div className="container">
+        <div className="site-title">Your Top Tracks</div>
+          <InfiniteScroll
+            className="cards"
+            pageStart={0}
+            loadMore={this.fetchTracks.bind(this)}
+            hasMore={this.props.tracks.length <= 40 && this.props.hasMore}
+            loader={<div className="loader">Loading...</div>}
+            threshold={500}
+          >
+            {tracks}
+          </InfiniteScroll>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-	return {
-		tracks: state.tracks,
-		hasMore: state.hasMoreTracks
-	};
+  return {
+    tracks: state.tracks,
+    hasMore: state.hasMoreTracks
+  };
 }
 
 export default connect(mapStateToProps, { fetchTracks })(Tracks);
