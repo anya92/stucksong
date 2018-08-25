961de4bf74d7f6db46a103226bf81e0e6b53ccfc
Ania <aniabrajtkopf@gmail.com>
2018-08-25 17:48:45 +0200

    created tests for PlaylistForm component

--- client/components/Playlist/PlaylistForm.test.js
+++ client/components/Playlist/PlaylistForm.test.js
@@ -24,4 +24,39 @@ describe('PlaylistForm component', () => {
   it('renders PlaylistForm correctly', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('calls handleChange on name change', () => {
+    const e = {
+      target: {
+        value: 'Top 10',
+      },
+    };
+    wrapper.find('#playlist-name').simulate('change', e);
+    expect(handleChange).toHaveBeenLastCalledWith('name', 'Top 10');
+  });
+
+  it('calls handleChange on description change', () => {
+    const e = {
+      target: {
+        value: 'My top 10 songs',
+      },
+    };
+    wrapper.find('#playlist-description').simulate('change', e);
+    expect(handleChange).toHaveBeenLastCalledWith('description', 'My top 10 songs');
+  });
+
+  it('calls handleChange on number of tracks change', () => {
+    const e = {
+      target: {
+        value: 10,
+      },
+    };
+    wrapper.find('#playlist-number-of-tracks').simulate('change', e);
+    expect(handleChange).toHaveBeenLastCalledWith('numberOfTracks', 10);
+  });
+
+  it('calls createPlaylist on form submit', () => {
+    wrapper.find('form').simulate('submit');
+    expect(createPlaylist).toHaveBeenCalled();
+  });
 });
